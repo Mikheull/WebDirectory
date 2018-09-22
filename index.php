835793fc2735fb207795e21651795fc9e495d448
Mikhael Bailly <8196641+Mikheull@users.noreply.github.com>
2018-09-22 18:04:06 +0200

    Incorrect css link (dev)

--- index.php
+++ index.php
@@ -69,8 +69,7 @@ $format_date = "j-n-Y H:i:s";
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdn.rawgit.com/Mikheull/WebDirectory/1f0b8a84/resources/themes/reset.css">
-    <!-- <link rel="stylesheet" href="https://cdn.rawgit.com/Mikheull/WebDirectory/1f0b8a84/resources/themes/<?= $theme ;?>.css"> -->
-    <link rel="stylesheet" href="resources/themes/dark.css">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/Mikheull/WebDirectory/1f0b8a84/resources/themes/<?= $theme ;?>.css">
 </head>
 
 
